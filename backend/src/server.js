0687a2774513df4fddb724927741a36c491a23ff
Isak Evaldsson <isak.evaldsson@outlook.com>
2020-09-04 20:06:10 +0200

    Built first version of notes restAPI.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
+//TODO schema validation
+
 app.use(express.json());
 
 const port = 3001;
+const url = '/api/notes'
 
 const notes = [
     { id: 1, name: 'favorite whiskes', content: ['Laphraoig', 'The Balveine', 'High Coast']}, 
@@ -11,26 +14,61 @@ const notes = [
     { id: 3, name: 'note to self', content: "Clean Kitchen"}
 ];
 
-app.get('/api/notes', function(req, res) {
+app.get(url, function(req, res) {
     res.send(notes)
 });
 
-app.get('/api/notes/:id', function(req, res){
+app.get(url + '/:id', function(req, res){
     const note = notes.find(n => n.id === parseInt(req.params.id))
     
-    if (!note) res.status(404).send('The note with the given ID was not found')
+    if (!note) return res.status(404).send('The note with the given ID was not found')    
+
     res.send(note)
 });
 
-app.post('/api/notes', (req, res) => {
+app.post(url, (req, res) => {
+    const name = req.body.name;
+    const content = req.body.content;
+
+    if(!name || name.length < 3) {
+        return res.status(400).send('Name is requiered and should be minum 3 characters')
+    }
+
     const note = {
         id: notes.length + 1,
         name: req.body.name,
-        content: req.body.content
+        content: !content ? '' : content 
     };
 
     notes.push(note);
     res.send(note);
 });
+
+app.put(url + '/:id', (req, res) => {
+    const note = notes.find(n => n.id === parseInt(req.params.id))
+    const name = req.body.name;
+    const content = req.body.content;
+    
+    if (!note) return res.status(404).send('The note with the given ID was not found')
+
+    if(name) {
+        note.name = name;
+    }
+    if(content) {
+        note.content = content
+    }
+    
+    res.send(note)
+});
+
+app.delete(url + '/:id', (req, res) => {
+    const note = notes.find(n => n.id === parseInt(req.params.id))
+    if (!note) return res.status(404).send('The note with the given ID was not found')
+
+    const index = notes.indexOf(note);
+    notes.splice(index, 1);
+
+    res.send(note)
+})
 
 app.listen(port, () => console.log(`Note app server listening on port ${port}...`))
